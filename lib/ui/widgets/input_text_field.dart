e922ed061a1a6cc7b4e790e45d884799ee0a30c7
abdelrahman <rain.forest5023@gmail.com>
2025-09-17 21:42:01 +0300

    Add InputTextField widget for user text input

---
+++ lib/ui/widgets/input_text_field.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class InputTextField extends StatelessWidget{
+  const InputTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
