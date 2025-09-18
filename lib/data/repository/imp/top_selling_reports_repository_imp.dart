9cc21be907b3488b006edcb51b06bc08b398e076
abdelrahman <rain.forest5023@gmail.com>
2025-09-18 15:29:00 +0300

    Refactor top selling reports to filter completed orders and rename service class

--- lib/data/repository/imp/top_selling_reports_repository_imp.dart
+++ lib/data/repository/imp/top_selling_reports_repository_imp.dart
@@ -1,7 +1,8 @@
 import 'package:assignment1/data/models/order_model.dart';
+import 'package:assignment1/data/models/constants/order_status.dart';
 import 'package:assignment1/data/repository/top_selling_reports_repository.dart';
 
-class TopSellingReportsRepositoryImp implements TopSellingReportsRepository{
+class TopSellingReportsRepositoryImp implements TopSellingReportsRepository {
   final List<OrderModel> orders;
 
   TopSellingReportsRepositoryImp(this.orders);
@@ -9,9 +10,11 @@ class TopSellingReportsRepositoryImp implements TopSellingReportsRepository{
   @override
   Map<String, int> topSellingItems() {
     final counts = <String, int>{};
-    for (var order in orders) {
-      counts[order.itemDrink.drinkName] =
-          (counts[order.itemDrink.drinkName] ?? 0) + order.quantity;
+    final completedOrders = orders.where((order) => order.orderStatus == OrderStatus.complete);
+
+    for (var order in completedOrders) {
+      counts[order.items.itemName] =
+          (counts[order.items.itemName] ?? 0) + order.quantity;
     }
     return counts;
   }
--- lib/domain/usecase/top_selling_reports_usecase.dart
+++ lib/domain/usecase/top_selling_reports_usecase.dart
@@ -1,9 +1,9 @@
 import '../../data/repository/top_selling_reports_repository.dart';
 
-class TopSellingReportsService {
+class TopSellingReportsUsecase {
   final TopSellingReportsRepository reportsRepository;
 
-  TopSellingReportsService(this.reportsRepository);
+  TopSellingReportsUsecase(this.reportsRepository);
 
   Map<String, int> topSellingItems() => reportsRepository.topSellingItems();
 }
